ae8953492b1259476c3b8e100c3a9f7654dcc04d
francesco97x <59744675+francesco97x@users.noreply.github.com>
2020-05-07 16:56:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -190,12 +190,7 @@ echo "<button id = 'firstbutton'  type='submit' name = 'submit' class='btn btn-p
   <!-- /.container -->
 
   <!-- Footer -->
-  <footer  id = "footer" class="py-5 bg-dark">
-    <div class="container">
- 
-    </div>
-    <!-- /.container -->
-  </footer>
+  
 
   <!-- Bootstrap core JavaScript -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
